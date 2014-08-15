9d2008124e1577e85b77ae5ccad2df4a8beebadc
Sophia Hoang <smhoang@cps.edu>
2014-08-15 15:12:49 +0000

    game complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Initialize Phaser Engine. Create a 400x490px game!
 
-var game = new Phaser.Game(400,490, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(4000,490, Phaser.AUTO, 'gameDiv');
 
 //Create our 'main' state that will contain the game
 //This is the body of the game itself
@@ -13,15 +13,13 @@ var mainState = {
     //Which is where we'll load our assets for the game 
     
     //Set the background color of the game 
-    game.stage.backgroundColor = "#1134d2";
+    game.stage.backgroundColor = "#71c5cf";
     
-    game.load.image('bird', 'assets/nemo.gif');
+    game.load.image('bird', 'assets/bird.png');
     
-    game.load.image('pipe', 'assets/jellyfish.gif');
-    
-    
+    game.load.image('pipe', 'assets/pipe.png');
+     
   },
-  
   
   create: function () {
     //This function is called right after perload function
@@ -29,7 +27,7 @@ var mainState = {
     
     game.physics.startSystem(Phaser.Physics.ARCADE);
     
-    this.bird = this.game.add.sprite(100, 245, 'bird');
+    this.bird = this.game.add.sprite(100, 250, 'bird');
     
     
     //Now that we have a bird and gravity...we need to tell the bird 
@@ -39,29 +37,36 @@ var mainState = {
     
     this.bird.body.gravity.y = 1000;
     
+    
     var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     
     spaceKey.onDown.add(this.jump, this);
     
     this.pipes = game.add.group();
     
+    //Add the body to the group
     this.pipes.enableBody = true;
-    
+   
+    //Create 20 pipes to hold in the group 
     this.pipes.createMultiple(20, 'pipe');
     
+    //Add in pipes over 1.5 seconds to the screen
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    
+    this.score = 0;
+    this.labelScore = game.add.text(20,20, "0", {font: "30px Arial", fill: "#ffffff"});
+    
 },
 
   update: function () {
     //This function runs 60 times per second
-    
-    
+    //It contains the lames logic and all time related actions
     //Check if the bird is outdside of the gamescreen
     if (this.bird.inWorld == false) {
       this.restartGame();
     }
     
-    
+    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
 },
 
 addOnePipe: function (x,y) {
@@ -80,12 +85,17 @@ addOnePipe: function (x,y) {
 addRowOfPipes: function () {
     var hole = Math.floor(Math.random() * 5) + 1;
     
+    
     for(var i = 0; i < 8; i++)
       if (i != hole && i != hole + 1){
         
         this.addOnePipe(400, i*60 + 10);
         
       }
+      
+      
+      this.score += 1;
+      this.labelScore.text = this.score; 
 },
 
   jump: function () {
